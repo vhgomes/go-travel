31dd4b9a4d629caf98df01432cc82c68711dd4fc
Victor Hugo <vhclaudiogomes@gmail.com>
2026-07-16 12:29:53 -0300

    consumer producer added

---
+++ internal/services/sqs/consumer.go
@@ -0,0 +1,84 @@
+package sqs
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
+)
+
+type MessageHandler func(ctx context.Context, message *types.Message) error
+
+type Consumer struct {
+	client   *sqs.Client
+	queueURL string
+}
+
+func NewConsumer(client *sqs.Client, queueURL string) *Consumer {
+	return &Consumer{
+		client:   client,
+		queueURL: queueURL,
+	}
+}
+
+func (c *Consumer) ReceiveMessages(ctx context.Context, maxMessages int32, waitTimeSeconds int32) ([]types.Message, error) {
+	if maxMessages > 10 {
+		maxMessages = 10
+	}
+
+	input := &sqs.ReceiveMessageInput{
+		QueueUrl:              aws.String(c.queueURL),
+		MaxNumberOfMessages:   maxMessages,
+		WaitTimeSeconds:       waitTimeSeconds,
+		MessageAttributeNames: []string{"All"},
+		AttributeNames:        []types.QueueAttributeName{"All"},
+	}
+
+	result, err := c.client.ReceiveMessage(ctx, input)
+	if err != nil {
+		return nil, fmt.Errorf("receiving messages: %w", err)
+	}
+
+	return result.Messages, nil
+}
+
+func (c *Consumer) DeleteMessage(ctx context.Context, receiptHandle string) error {
+	input := &sqs.DeleteMessageInput{
+		QueueUrl:      aws.String(c.queueURL),
+		ReceiptHandle: aws.String(receiptHandle),
+	}
+
+	_, err := c.client.DeleteMessage(ctx, input)
+	if err != nil {
+		return fmt.Errorf("deleting message: %w", err)
+	}
+
+	return nil
+}
+
+func (c *Consumer) Poll(ctx context.Context, handler MessageHandler, maxMessages int32, waitTimeSeconds int32) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			messages, err := c.ReceiveMessages(ctx, maxMessages, waitTimeSeconds)
+			if err != nil {
+				return err
+			}
+
+			for _, msg := range messages {
+				if err := handler(ctx, &msg); err != nil {
+					fmt.Printf("error handling message %s: %v\n", *msg.MessageId, err)
+					continue
+				}
+
+				if err := c.DeleteMessage(ctx, *msg.ReceiptHandle); err != nil {
+					fmt.Printf("error deleting message %s: %v\n", *msg.MessageId, err)
+				}
+			}
+		}
+	}
+}
---
+++ internal/services/sqs/producer.go
@@ -0,0 +1,46 @@
+package sqs
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
+)
+
+type Producer struct {
+	client   *sqs.Client
+	queueURL string
+}
+
+func NewProducer(client *sqs.Client, queueURL string) *Producer {
+	return &Producer{
+		client:   client,
+		queueURL: queueURL,
+	}
+}
+
+func (p *Producer) SendMessage(ctx context.Context, body string, attributes map[string]string) (string, error) {
+	messageAttributes := make(map[string]types.MessageAttributeValue)
+
+	for key, value := range attributes {
+		messageAttributes[key] = types.MessageAttributeValue{
+			StringValue: aws.String(value),
+			DataType:    aws.String("String"),
+		}
+	}
+
+	input := &sqs.SendMessageInput{
+		QueueUrl:          aws.String(p.queueURL),
+		MessageBody:       aws.String(body),
+		MessageAttributes: messageAttributes,
+	}
+
+	result, err := p.client.SendMessage(ctx, input)
+	if err != nil {
+		return "", fmt.Errorf("sending message: %w", err)
+	}
+
+	return *result.MessageId, nil
+}
