c23697e33a2f6089144e57ea43c82ad51f5dd7c2
Victor Hugo <vhclaudiogomes@gmail.com>
2026-07-16 12:41:10 -0300

    set header

--- internal/services/order/handler.go
+++ internal/services/order/handler.go
@@ -64,6 +64,7 @@ func (h *OrderHandler) CreateOrder(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	_ = json.NewEncoder(w).Encode(map[string]string{"id": order.ID.String()})
 }
